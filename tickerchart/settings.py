7255c56085f8f6025b8b35714f068be3029bb02c
Work <muzammilsiddiqui463@gmail.com>
2024-01-01 22:14:37 +0500

    update

--- tickerchart/settings.py
+++ tickerchart/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1dw(d5%&u*uhp5aw4c*1w=&qf9$8@hbw-m2=&8om3nytt&p)2y
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-
+CSRF_TRUSTED_ORIGINS = ['*']
 
 # Application definition
 
