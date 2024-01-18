2e2aae52ec3cb3c71f45bec8cb084871ebf0da2b
Work <muzammilsiddiqui463@gmail.com>
2024-01-19 02:18:43 +0500

    update

--- test.py
+++ test.py
@@ -10,14 +10,14 @@ import os
 # Define Chrome options
 chrome_options = Options()
 # chrome_options.add_argument('--headless')  # Run Chrome in headless mode
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+# chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 # driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH")
 
 # Initialize the WebDriver with Chrome options
-driver = uc.Chrome(options=chrome_options, driver_executable_path=os.environ.get("CHROMEDRIVER_PATH"))
+driver = uc.Chrome(options=chrome_options)
 driver.maximize_window()
 driver.get("https://www.tickerchart.net/app/en#")
 driver.close()
