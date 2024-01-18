d4deb1ff0dc8406ea2830d763412a2c8eb9f65fe
Work <muzammilsiddiqui463@gmail.com>
2024-01-19 04:42:01 +0500

    configs

---
+++ conf/gunicorn_config.py
@@ -0,0 +1,4 @@
+commad = '/home/muzammil/ticketchart_project/tickerchart_api/venv/bin/gunicorn'
+path = '/home/muzammil/ticketchart_project/tickerchart_api'
+bind = '135.181.92.180:8000'
+workers = 3
