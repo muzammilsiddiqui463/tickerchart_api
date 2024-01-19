72200cdbd7c9e701e820c7a8a6e861fe55e1993c
Work <muzammilsiddiqui463@gmail.com>
2024-01-19 05:52:17 +0500

    updates

--- tickerchartapp/bot/scrapper.py
+++ tickerchartapp/bot/scrapper.py
@@ -22,14 +22,14 @@ class Scraper:
         # Define Chrome options
         chrome_options = Options()
         # chrome_options.add_argument('--headless')  # Run Chrome in headless mode
-        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
         # driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH")
     
         # Initialize the WebDriver with Chrome options
-        self.driver = uc.Chrome(options=chrome_options,driver_executable_path=os.environ.get("CHROMEDRIVER_PATH"))
+        self.driver = uc.Chrome(options=chrome_options)
         self.driver.maximize_window()
         self.driver.get("https://www.tickerchart.net/app/en#")
         self.wait = WebDriverWait(self.driver, 25)
