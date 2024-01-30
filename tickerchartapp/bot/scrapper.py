326ed8a50cad8028da112039c97fab1fb286f0a2
Work <muzammilsiddiqui463@gmail.com>
2024-01-30 17:23:25 +0500

    updates

--- tickerchartapp/bot/scrapper.py
+++ tickerchartapp/bot/scrapper.py
@@ -10,6 +10,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.options import Options
 import undetected_chromedriver as uc
 import requests
+from bs4 import BeautifulSoup
 from ..host import HOST_URL
 import pickle
 import os
@@ -101,18 +102,26 @@ class Scraper:
     def table_data(self):
         results = []
         def sub_process(row_data):
-
-
             try:
-                # if row_data.text.split('\n') in history:
-                #     return
-                # else:
-                #     # print(row.text.split('\n'))
-                results.append(row_data.text.split('\n'))
-                return row_data.text.split('\n')
-            except Exception as e:
-                print(e)
+                col = row_data.select("div[class*='slick-cell']")
+                temp = []
+                for c in col:
+                    temp.append(c.text)
+                results.append(temp)
+            except:
                 pass
+
+
+            # try:
+            #     # if row_data.text.split('\n') in history:
+            #     #     return
+            #     # else:
+            #     #     # print(row.text.split('\n'))
+            #     results.append(row_data.split('\n'))
+            #     return row_data.split('\n')
+            # except Exception as e:
+            #     print(e)
+            #     pass
         wait = WebDriverWait(self.driver, 30)
         # self.driver.execute_script("document.body.style.zoom='25%'")
         actions = ActionChains(self.driver)
@@ -132,13 +141,43 @@ class Scraper:
                 # print(current_height,"==current height")
                 for _ in range(0,2):
                     # initial_height = current_height
-                    rows = self.driver.find_elements(By.XPATH,
-                                                     "//marketwatch//div[@class='grid-canvas']//div[contains(@class,'slick-row')]")
-                    print(len(rows))
+                    # rows = self.driver.find_elements(By.XPATH,
+                    #                                  "//marketwatch//div[@class='grid-canvas']//div[contains(@class,'slick-row')]")
+                    # print(len(rows))
                     threads  =[]
-                    for row in rows:
+                    # Get the complete HTML content of the page
+                    complete_html =self.driver.page_source
+
+                    # Store the HTML content somewhere (you can save it to a file, a variable, or a database)
+                    # Example: Save to a file
+                    with open("page_content.html", "w", encoding="utf-8") as file:
+                        file.write(complete_html)
+                    # for r in range(0,len(rows)):
+                    #     try:
+                    #         row = self.driver.find_elements("xpath",f"//marketwatch//div[@class='grid-canvas']//div[contains(@class,'slick-row')]")[r].text
+                    #         # print(row)
+                    #     except:
+                    #         continue
+                    #
+                    #     s = threading.Thread(target=sub_process,args=(row,))
+                    #     threads.append(s)
+
+                    # Load the saved HTML content
+                    with open("page_content.html", "r", encoding="utf-8") as file:
+                        saved_html = file.read()
+
+                    # Parse the HTML using BeautifulSoup
+                    soup = BeautifulSoup(saved_html, 'lxml')
+
+                    # Find elements using your XPath expression
+                    elements = soup.select("div.grid-canvas div[class*='slick-row']")
+                    print(len(elements))
+                    # Perform actions on the found elements
+                    for element in elements:
+                        row = element
                         s = threading.Thread(target=sub_process,args=(row,))
                         threads.append(s)
+
 
 
                         # try:
@@ -161,7 +200,7 @@ class Scraper:
                     # self.driver.execute_script("window.scrollTo(0, window.scrollY + 2000)")
                     # new_scroll_position = self.driver.execute_script("return window.scrollY")
                     actions.send_keys_to_element(scroll, Keys.PAGE_DOWN).perform()
-                    time.sleep(3)
+                    time.sleep(1)
                     # current_height = self.driver.execute_script("return arguments[0].scrollHeight;", scroll)
             except Exception as e:
                 print(e)
@@ -175,9 +214,9 @@ class Scraper:
                     # print(element)
                     # print(element)
                     # print(f"Symobol: {element[0]}, Close: {element[2]}, Open: {element[4]}, High: {element[5]}, Low: {element[6]}, Volume: {element[7]}")
-                    data = {"Symbol": element[0], "close": element[2], "open": element[4], "high": element[5],
-                            "low": element[6], "volume": element[7]}
-                    # print(data)
+                    data = {"Symbol": element[3], "close": element[5], "open": element[18], "high": element[19],
+                            "low": element[20], "volume": element[15]}
+                    print(data)
                     all_data.append(data)
                     # add = requests.post("http://127.0.0.1:8000/data/", data=data).json()
                 except:
@@ -187,7 +226,7 @@ class Scraper:
             def add_delete(all_data):
                 delete_data = requests.delete(f"{HOST_URL}data/")
                 add = requests.post(f"{HOST_URL}data/", json=all_data)
-                # print(add.json())
+                print(add.json())
             print(len(all_data),"===all data")
             s = threading.Thread(target=add_delete, args=(all_data,))
             s.start()
